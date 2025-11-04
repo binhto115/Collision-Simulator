0a93412b14bb3419503840e5858e1720c8c98f1c
Jeremy Huang <jeremyhuangofficial@gmail.com>
2025-11-03 20:37:03 -0800

    added account-model.js, for db collection

---
+++ packages/express-backend/models/account-model.js
@@ -0,0 +1,12 @@
+// /models/account-model.js
+// defines "accounts" collection in MongoDB Atlas cluster
+import mongoose from "mongoose";
+
+const accountSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+const Account = mongoose.model("Account", accountSchema);
+
+export default Account;
