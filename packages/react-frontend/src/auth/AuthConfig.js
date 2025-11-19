e11f99fb10ea72dcba857b6a5401f463cff188f4
Jeremy Huang <jeremyhuangofficial@gmail.com>
2025-11-18 22:35:48 -0800

    https to the front

--- packages/react-frontend/src/auth/AuthConfig.js
+++ packages/react-frontend/src/auth/AuthConfig.js
@@ -1,2 +1,2 @@
 export const API_BASE =
-    import.meta.env.VITE_API_BASE || "collision-sim-backend-hwexdffvd3c5c7e9.westus3-01.azurewebsites.net";
+    import.meta.env.VITE_API_BASE || "https://collision-sim-backend-hwexdffvd3c5c7e9.westus3-01.azurewebsites.net";
